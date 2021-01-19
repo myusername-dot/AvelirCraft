03d3a000201e082a3769ab9e68da7d569a98a444
Andrej <andreiiljuhin@gmail.com>
2021-01-19 23:07:18 +0300

    tests p3

--- src/test/java/com/avelircraft/HomeControllerTest.java
+++ src/test/java/com/avelircraft/HomeControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -62,5 +64,58 @@ public class HomeControllerTest {
                 .andExpect(xpath("//div[@id='block-content']/div").nodeCount(10))
                 .andExpect(xpath("//div[@id='block-content']/div[@data-id=3]").exists())
                 .andExpect(xpath("//div[@id='block-content']/div[@data-id=5]").exists());
+    }
+
+    @Test
+    @Sql(value = {"/create-guide-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/delete-guide-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void guideMenuPageFull() throws Exception {
+        this.mockMvc.perform(get("/guidmenu"))
+                .andExpect(status().isOk())
+                .andExpect(xpath("//div[@id='guides']/div").nodeCount(3))
+                // На самом деле Header №3 будет снизу, а Гайд 1 сверху, так уж написал фронтендер
+                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Header №3")))
+                .andExpect(xpath("//div[@id='guides']/div[2]/div[1]").string(containsString("Заголовок гайда №2")))
+                .andExpect(xpath("//div[@id='guides']/div[3]/div[1]").string(containsString("Гайд 1")));
+    }
+
+    @Test
+    @Sql(value = {"/create-guide-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/delete-guide-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+    public void guideMenuPageTags() throws Exception {
+        // Why does not it work?
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "два+3три"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(2))
+//                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Заголовок гайда №2")))
+//                .andExpect(xpath("//div[@id='guides']/div[2]/div[1]").string(containsString("Гайд 1")));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "odin1"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(1))
+//                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Гайд 1")));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "3три+odin1"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(1))
+//                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Гайд 1")));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "odin1+3три"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(1))
+//                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Гайд 1")));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "4four4"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(1))
+//                .andExpect(xpath("//div[@id='guides']/div[1]/div[1]").string(containsString("Header №3")));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "odin1+4four4"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(0));
+//
+//        this.mockMvc.perform(get("/guidmenu").param("tags", "null"))
+//                .andExpect(status().isOk())
+//                .andExpect(xpath("//div[@id='guides']/div").nodeCount(0));
     }
 }
---
+++ src/test/resources/create-guide-before.sql
@@ -0,0 +1,6 @@
+delete from guide;
+
+INSERT INTO `guide` (`id`, `link`, `header`, `description`, `tags`, `date`, `views`) VALUES
+(4, 'https://www.youtube-nocookie.com/embed/gtgO9y3LbiY', 'Гайд 1', 'Описание для проверки', ' odin1 два 3три ', '2021-01-19 17:08:46', 0),
+(5, 'https://www.youtube-nocookie.com/embed/gtgO9y3LbiY', 'Заголовок гайда №2', 'description', ' два 3три ', '2021-01-19 17:09:57', 10),
+(6, 'https://www.youtube-nocookie.com/embed/gtgO9y3LbiY', 'Header №3.', 'desc.', ' 4four4 ', '2021-01-19 17:11:59', 0);
