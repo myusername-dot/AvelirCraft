e8806a75f30e35e94385ea4a16e1a038094d0ab3
Andrej <you@example.com>
2021-07-01 16:12:30 +0300

    add a user to the database before checking the login

--- src/test/java/com/avelircraft/LoginTest.java
+++ src/test/java/com/avelircraft/LoginTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -44,6 +45,8 @@ public class LoginTest {
     }
 
     @Test
+    @Sql(value = {"/create-user-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(value = {"/delete-user-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
     public void correctLogin() throws Exception {
         this.mockMvc.perform(formLogin().user("Dante").password("passwortt"))
                 .andDo(print())
